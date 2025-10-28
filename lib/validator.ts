0cf2cd0270e9092e55181126dbc6793a9f6ca907
Ayush Kumar <rajaayush124212@gmail.com>
2025-10-28 10:34:53 +0530

    minor bug fixed

--- lib/validator.ts
+++ lib/validator.ts
@@ -2,14 +2,13 @@ import { z } from "zod";
 
 // the form schema
 export  const eventFormSchema = z.object({
-    title:z.string().min(3, 'title must be at least 3 charchters').max(20,'title must be less than 20 charchters'),
-    description:z.string().min(3, 'description must be at least 3 charchters').max(400,'description must be less than 400 charchters'),
-    location:z.string().min(3, 'location must be at least 3 charchters').max(30,'location must be less than 400 charchters'),
+    title: z.string().min(3, 'title must be at least 3 characters').max(40,'title must be less than 40 characters'),    description:z.string().min(3, 'description must be at least 3 charchters').max(600,'description must be less than 600 charchters'),
+    location:z.string().min(3, 'location must be at least 3 charchters').max(50,'location must be less than 50 charchters'),
     imageUrl:z.string(),
     startDateTime:z.date(),
     endDateTime:z.date(),
     categoryId:z.string(),
     price:z.string(),
     isFree:z.boolean(),
-    url:z.string().url(),
+    url:z.string().url().or(z.literal('')).optional(),
 });
