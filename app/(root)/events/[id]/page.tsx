d297fbc52c0b0fe9919597551ede3de7e3d53f1b
Ayush Kumar <rajaayush124212@gmail.com>
2025-11-06 14:53:30 +0530

    fixed event dispay page

--- app/(root)/events/[id]/page.tsx
+++ app/(root)/events/[id]/page.tsx
@@ -20,70 +20,75 @@ const EventDetails = async (props: PageProps<'/events/[id]'>)=> {
 
   return (
     <>
-        <section className="flex justify-center bg-primary-50 bg-dotted-pattern bg-contain">
-      <div className="grid grid-cols-1 md:grid-cols-2 2xl:max-w-7xl">
-        <Image src={event.imageUrl}
-         alt="event-img" width={1000} height={1000}
-         className="h-full min-h-[300px]
-         object-cover object-center"></Image>
+<section className="flex justify-center bg-primary-50 bg-dotted-pattern bg-cover">
+  <div className="grid grid-cols-1 md:grid-cols-2 2xl:max-w-7xl">
+    <Image 
+      src={event.imageUrl}
+      alt="event-img" 
+      width={1000} 
+      height={1000}
+      className="h-full min-h-[300px] object-contain object-center"
+    />
 
-         <div className="flex w-full flex-col gap-8 p-5 md:p-10">
-          <div className="flex flex-col gap-6">
-            <h2 className="text-2xl font-bold">{event.title}</h2>
-            <div className="flex flex-col gap-3 sm:flex-row
-            sm:items-center">
-              <div className="flex gap-3">
-                <p className="font-bold text-l rounded-full bg-green-500/10
-                px-5 py-2 text-green-700">
-                  {event.isFree? 'Free':`₹${event.price}`}
-                </p>
-                <p className="font-medium text-base rounded-full bg-gray-500/10
-                px-4 py-2.5 text-gray-500">
-                  {event.category.name}
-                </p>
-              </div>
-              <p className="font-medium text-lg ml-2 mt-2 sm:mt-0">
-                by {''}
-                <span className="text-blue-500">{event.organizer.firstName} {event.organizer.lastName}</span>
-              </p>
-            </div>
+    <div className="flex w-full flex-col gap-8 p-5 md:p-10">
+      <div className="flex flex-col gap-6">
+        <h2 className="text-2xl font-bold">{event.title}</h2>
+        <div className="flex flex-col gap-3 sm:flex-row sm:items-center">
+          <div className="flex gap-3">
+            <p className="font-bold text-lg rounded-full bg-green-500/10 px-5 py-2 text-green-700">
+              {event.isFree ? 'FREE' : `₹${event.price}`}
+            </p>
+            <p className="font-medium text-base rounded-full bg-gray-500/10 px-4 py-2.5 text-gray-500">
+              {event.category.name}
+            </p>
           </div>
-          {/* CHECKOUT BUTTON */}
-          <CheckoutButton event={event}/>
-
-          <div className="flex flex-col gap-5">
-            <div className="flex gap-2 md:gap-3">
-              <Image src="/assets/icons/calendar.svg" alt="calendar"
-              height={32} width={32}/>
-              <div className="p-medium-16 lg:p-regular-20
-               flex flex-col flex-wrap  items-center">
-              <p>
-                {formatDateTime(event.startDateTime).dateOnly}-{' '}
-                {formatDateTime(event.startDateTime).timeOnly} 
-              </p>
-              <p className="ml-1">
-                {formatDateTime(event.endDateTime).dateOnly}-{' '}
-                {formatDateTime(event.endDateTime).timeOnly}
-              </p>
-              </div>
-            </div>
-            <div className="flex items-center gap-3 font-medium text-lg">
-              <Image src="/assets/icons/location.svg"
-               alt="location" height={32} width={32}/>
-               <p className="font-medium text-lg lg:font-medium">{event.location}</p>
-            </div>
-          </div>
-          <div className="flex flex-col gap-2">
-            <p className="font-bold text-lg text-gray-600">Desctiption:</p>
-            <p className="font-normal lg:font-normal">{event.description}</p>
-            <Link href={event.url}>
-            <p className="font-normal lg:font-normal text-lg
-             truncate underline text-blue-400">{event.url}</p>
-            </Link>
-          </div>
-         </div>
+          <p className="font-medium text-lg ml-2 mt-2 sm:mt-0">
+            by {''}
+            <span className="text-blue-500">{event.organizer.firstName} {event.organizer.lastName}</span>
+          </p>
+        </div>
       </div>
-    </section>
+      
+      {/* CHECKOUT BUTTON */}
+      <CheckoutButton event={event} />
+
+      <div className="flex flex-col gap-5">
+        <div className="flex gap-2 md:gap-3">
+          <Image src="/assets/icons/calendar.svg" alt="calendar" height={32} width={32} />
+          <div className="font-medium text-base lg:font-normal lg:text-xl flex flex-col flex-wrap items-start">
+            <p>
+              {formatDateTime(event.startDateTime).dateOnly} - {' '}
+              {formatDateTime(event.startDateTime).timeOnly}
+            </p>
+            <p>
+              {formatDateTime(event.endDateTime).dateOnly} - {' '}
+              {formatDateTime(event.endDateTime).timeOnly}
+            </p>
+          </div>
+        </div>
+        <div className="flex items-center gap-3 font-medium text-lg">
+          <Image src="/assets/icons/location.svg" alt="location" height={32} width={32} />
+          <p>{event.location}</p>
+        </div>
+      </div>
+      
+      <div className="flex flex-col gap-2">
+        <p className="font-bold text-lg text-gray-600">Description:</p>
+        <p className="font-normal lg:font-normal">{event.description}</p>
+        
+        {event.url && (
+          <Link 
+            href={event.url} 
+            target="_blank" 
+            className="font-normal lg:font-normal text-lg truncate underline text-blue-400 hover:text-blue-500"
+          >
+            {event.url}
+          </Link>
+        )}
+      </div>
+    </div>
+  </div>
+</section>
 
     {/*Events from the same category*/ }
     <section className="wrapper my-8 flex flex-col
